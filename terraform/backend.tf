f3d2d220cd1532fe33fff4a55713fa4c293f38d1
Joel Sánchez Simón <jsa5214@office.proven.cat>
2026-03-01 03:31:08 +0100

    Docker deploy integration & various fixes

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-364931100987-project"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
